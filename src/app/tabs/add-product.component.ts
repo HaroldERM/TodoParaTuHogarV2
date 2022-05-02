e1740ad67d020ac754a9c793122a9fc2dda832c2
HaroldERM <enrique5129876@estudiantec.cr>
2022-05-01 18:56:13 -0600

    1 file modified

--- src/app/tabs/add-product.component.ts
+++ src/app/tabs/add-product.component.ts
@@ -1,35 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { ProductsService } from '../products.service';
 
-
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.css']
+  styleUrls: ['./add-product.component.css'],
 })
 export class AddProductComponent implements OnInit {
-  public itemForm:FormGroup
+  public itemForm: FormGroup;
   constructor(
-    public productsService:ProductsService,
+    public productsService: ProductsService,
     public formBuilder: FormBuilder,
     public router: Router
   ) {
     this.itemForm = this.formBuilder.group({
-      nombre : [''],
-      precio : [''],
-      fecha : [''],
-      desc : [''],
-    })
+      nombre: [''],
+      precio: [''],
+      fecha: [''],
+      desc: [''],
+    });
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  onSubmit(){
-    this.productsService.createProduct(this.itemForm.value)
-    this.router.navigate([''])
+  onSubmit() {
+    this.productsService.createProduct(this.itemForm.value);
+    this.router.navigate(['products']);
   }
 }
